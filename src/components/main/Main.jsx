bc6cab6cd9951a2adf294e6d538077e7934eea67
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-04 15:30:02 +0100

    change the post card and the component card

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,84 +1,158 @@
+import axios from "axios";
 import style from "./Main.module.css";
 import { PostCard } from "./PostCard.jsx";
 import { useEffect, useState } from "react";
 
-let basePost = {
-  id:4,
+const basePost = {
   title: "",
   slug: "",
   content: ``,
   image: "",
   tags: [],
-}
+};
 
 export function Main() {
-  const [formData, setFormData] = useState(basePost) //creiamo una varibile statica che ha come valore iniziale l'oggetto creato sopra
-  const [posts, setPosts] = useState([])
-  const [status, setStatus] = useState(false)
- 
+  const [formData, setFormData] = useState(basePost); //creiamo una varibile statica che ha come valore iniziale l'oggetto creato sopra
+  const [posts, setPosts] = useState([]);
+  const [status, setStatus] = useState(false);
+  const uri = "http://localhost:3000/posts/";
+
   //funzione che mi aggiorna i valori del form data al cambio degli input
-  function changeHandler(event){
-    //creo una variabile per il valore da mettere nell'oggetto, se l'elemento che ha scatenato l'evento è  di tipo checkbox 
+  function changeHandler(event) {
+    //creo una variabile per il valore da mettere nell'oggetto, se l'elemento che ha scatenato l'evento è  di tipo checkbox
     //allora il valore andra a prendere il valore di checked altrimenti di value
 
-    let value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+    let value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
     /*if(event.target.type === 'checkbox'){
       
     }*/
-    setFormData((formData)=>{ //passo una callback alla funzione set che ha come parametro la variabile reattiva
-      return {...formData, //ritornera un oggetto copia di form data
-        [event.target.name]:value} //ma che avra una key presa dal name dell elemento che ha scatenato l'evento con il rispettivo valore come value
-    })
+    setFormData((formData) => {
+      //passo una callback alla funzione set che ha come parametro la variabile reattiva
+      return {
+        ...formData, //ritornera un oggetto copia di form data
+        [event.target.name]: value,
+      }; //ma che avra una key presa dal name dell elemento che ha scatenato l'evento con il rispettivo valore come value
+    });
   }
 
-  function onSubmit(event){
+  function onSubmit(event) {
     event.preventDefault();
     //controllo se i campi non sono vuoti
-    if(formData.title === '' && formData.content === ''){
-      console.log('vuoto uno dei due')
-      return
+    if (formData.title === "" && formData.content === "") {
+      console.log("vuoto uno dei due");
+      return;
     }
-    
 
     //creo una variabile di appoggio dove andro a mettere i valori che ho preso dal form
     const newPost = {
-      ...formData,//uso lo spread operator per mettere i valori di form data dentro al nuovo post
-    }
-  
-    setPosts([...posts, newPost])//con set new arr vado a creare un array con i vecchi post piu quello nuovo
-    setFormData(basePost)//riazzero i campi
-    setStatus(false)
+      ...formData, //uso lo spread operator per mettere i valori di form data dentro al nuovo post
+    };
+
+    setPosts([...posts, newPost]); //con set new arr vado a creare un array con i vecchi post piu quello nuovo
+    setFormData(basePost); //riazzero i campi
+    setStatus(false);
   }
 
-  function deletePost(postToDelete){ //function per eliminare un post
-    setPosts(posts.filter(post=>post!== postToDelete)) //prendo tutto tranne il post che corrisponde al post che ho passato alla funzione
+  function deletePost(postToDelete) {
+    //function per eliminare un post
+    setPosts(posts.filter((post) => post !== postToDelete)); //prendo tutto tranne il post che corrisponde al post che ho passato alla funzione
+  }
+
+  function axiosPostsCall() {
+    axios
+      .get(uri)
+      .then((res) => {
+        console.log(res.data);
+        setPosts(res.data);
+        console.log(posts);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   useEffect(() => {
-    if(status){
-      alert("Status aggiornato")
-    }
-  }, [status])
+    axiosPostsCall();
+  }, []);
 
+  useEffect(() => {
+    if (status) {
+      alert("Status aggiornato");
+    }
+  }, [status]);
 
   return (
     <div className={[style.bgcolor_lightGrey, style.flex_grow_1].join(" ")}>
       <div className="container">
-          <form className="row column-gap-3 row-gap-3 pt-5" onSubmit ={onSubmit} action="">
-            {/* vado a mettere nei campi di input la funzione da invocare al cambio dell'input e come valore il valore della variabile reattiva*/}
-            <input onChange={(e)=>changeHandler(e)} type="text" className="col-2" name="title" value={formData.title} placeholder="inserisci titolo"></input> 
-            <input onChange={(e)=>changeHandler(e)} type="text" className="col-2" name="slug" value={formData.slug} placeholder="inserisci testo"></input>
-            <input onChange={(e)=>changeHandler(e)} type="text" className="col-2" name="content" value={formData.content} placeholder="inserisci contenuto"></input>
-            <input onChange={(e)=>changeHandler(e)} type="text" className="col-2" name="image" value={formData.image} placeholder="inserisci url imagine"></input>
-            <input onChange={(e)=>changeHandler(e)} type="text" className="col-2" name="tags" value={formData.tags} placeholder="inserisci tags"></input>
-            <input type="submit" className="col-1" onClick={()=>{setStatus(true)}}></input>
-          </form>
-        <div
-          className={[style.justify_center,style.align_center,style.padding_top_50,style.row_gap_15]}
+        <form
+          className="row column-gap-3 row-gap-3 pt-5"
+          onSubmit={onSubmit}
+          action=""
         >
-          {posts.map((el) => {
-            return (
-              el.published && (
+          {/* vado a mettere nei campi di input la funzione da invocare al cambio dell'input e come valore il valore della variabile reattiva*/}
+          <input
+            onChange={(e) => changeHandler(e)}
+            type="text"
+            className="col-2"
+            name="title"
+            value={formData.title}
+            placeholder="inserisci titolo"
+          ></input>
+          <input
+            onChange={(e) => changeHandler(e)}
+            type="text"
+            className="col-2"
+            name="slug"
+            value={formData.slug}
+            placeholder="inserisci testo"
+          ></input>
+          <input
+            onChange={(e) => changeHandler(e)}
+            type="text"
+            className="col-2"
+            name="content"
+            value={formData.content}
+            placeholder="inserisci contenuto"
+          ></input>
+          <input
+            onChange={(e) => changeHandler(e)}
+            type="text"
+            className="col-2"
+            name="image"
+            value={formData.image}
+            placeholder="inserisci url imagine"
+          ></input>
+          <input
+            onChange={(e) => changeHandler(e)}
+            type="text"
+            className="col-2"
+            name="tags"
+            value={formData.tags}
+            placeholder="inserisci tags"
+          ></input>
+          <input
+            type="submit"
+            className="col-1"
+            onClick={() => {
+              setStatus(true);
+            }}
+          ></input>
+        </form>
+        <div
+          className={[
+            style.justify_center,
+            style.align_center,
+            style.padding_top_50,
+            style.row_gap_15,
+          ]}
+        >
+          {
+          posts.length !== 0 ? (
+            posts.map((el) => {
+              return (
                 <PostCard
                   key={el.id}
                   id={el.id}
@@ -86,12 +160,13 @@ export function Main() {
                   image={el.image}
                   content={el.content}
                   tags={el.tags}
-                  published={el.published}
-                  callback = {()=>deletePost(el)}
+                  callback={() => deletePost(el)}
                 />
-              )
-            );
-          })}
+              );
+            })
+          ) : (
+            <div>non ci sono post</div>
+          )}
         </div>
       </div>
     </div>
--- src/components/main/PostCard.jsx
+++ src/components/main/PostCard.jsx
@@ -1,41 +1,18 @@
 import sconsolato from "../../assets/sconsolato.jpg"
 
-function getCssTags(tag) {
-  switch (tag) {
-    case "html":
-      return {
-        backgroundColor: "#FE4C1E",
-      };
-    case "css":
-      return{
-        backgroundColor: "#0071BF",
-    };
-    case "js":
-      return{
-        backgroundColor: "#F7DF1E",
-    };
-    case "php":
-      return{
-        backgroundColor: "#777BB3",
-    };
-    default:
-      return {};
-  }
-}
-
 export function PostCard(props) {
-  const {id = "", title = "", image = "", content = "", tags = [], published = false, callback, callbackChangeTitle} = props;
+  const {id = "", title = "", image = "", content = "", tags = [], callback} = props;
 
   return (
-    <div className="col-4">
+    <div className="col-6">
       <div className="card" id={`${id}`}>
-        <img src={image ? image : sconsolato} className="card-img-top" alt="..." />
+        <img src={image ? 'http://localhost:3000/'+image : sconsolato} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           
           <p>
             {tags.length != 0 ? tags.map((tag) => (
-              <span key={tag}>&nbsp;<span  style={getCssTags(tag)}> {tag} </span>&nbsp;</span>
+              <span key={tag}>&nbsp;<span> {tag} </span>&nbsp;</span>
             )) : <span>niente tags</span>}
            
           </p>
@@ -43,13 +20,8 @@ export function PostCard(props) {
           <div className="row">
             <div className="col">
               <a href="#" className="btn btn-primary" onClick={callback}>remove</a>
-              </div>
-            <div className="col">
-              <a href="#" className="btn btn-primary" onClick={callbackChangeTitle}>change title</a>
             </div>
           </div>
-          
-          
         </div>
       </div>
     </div>
