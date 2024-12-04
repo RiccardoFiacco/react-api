7aa6d7437578829cb2d66a12608b550826831c34
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-04 13:57:17 +0100

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header } from "./components/header/Header";
+import style from "./App.module.css";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    
+    <div className={`d-flex flex-column ${style.vh100}`}>
+      <Header />
+      
+    </div>
   )
 }
 
